478ddf176015fc6a13909ac155aa407fd2ab6c6c
matheuscgc <matheuscgc@al.insper.edu.br>
2016-04-19 21:33:29 -0300

    Interface do tabuleiro funcionando dentro de uma classe, porem ainda não marca no botão se foi escolhido x ou o

--- Tabuleiro.py
+++ Tabuleiro.py
@@ -1,50 +1,61 @@
 import tkinter as tk
-from tkinter import *
 class Tabuleiro:
-	def __init__ (self, botao):
-		self.botao = botao
-	def selec_x():
+	def __init__ (self):
+		self.window=tk.Tk()
+		self.window.title("Jogo da velha!")
+		botao1=tk.Button(self.window)
+		botao1.grid(row=0, column=0)
+		botao1.config(height=10, width=20)
+		#botao1.configure(command=selec_x)
+		#botao1["text"]="X"
+		botao1.bind("<Button-1>",selec_x)
+
+		botao2=tk.Button(self.window)
+		botao2.grid(row=0, column=1)
+		botao2.config(height=10, width=20)
+
+		botao3=tk.Button(self.window)
+		botao3.grid(row=0, column=2)
+		botao3.config(height=10, width=20)
+
+		botao4=tk.Button(self.window)
+		botao4.grid(row=1, column=0)
+		botao4.config(height=10, width=20)
+	
+		botao5=tk.Button(self.window)
+		botao5.grid(row=1, column=1)
+		botao5.config(height=10, width=20)
+
+		botao6=tk.Button(self.window)
+		botao6.grid(row=1, column=2)
+		botao6.config(height=10, width=20)
+
+		botao7=tk.Button(self.window)
+		botao7.grid(row=2, column=0)
+		botao7.config(height=10, width=20)
+	
+		botao8=tk.Button(self.window)
+		botao8.grid(row=2, column=1)
+		botao8.config(height=10, width=20)
+
+		botao9=tk.Button(self.window)
+		botao9.grid(row=2, column=2)
+		botao9.config(height=10, width=20)
+
+	def iniciar(self):
+		self.window.mainloop()
+
+	def selec_x(event):
+		#["text"]="X"
 		
 		
-	def selec_o():
-		
-		
-		
-window=tk.Tk()
-botao1=tk.Button(window)
-botao1.grid(row=0, column=0)
-botao1.config(height=10, width=20)
+	def selec_o(event):
+		#["text"]="O"
+	
+	
+	def escolha(self):
+		return False
 
-botao2=tk.Button(window)
-botao2.grid(row=0, column=1)
-botao2.config(height=10, width=20)
-
-botao3=tk.Button(window)
-botao3.grid(row=0, column=2)
-botao3.config(height=10, width=20)
-
-botao4=tk.Button(window)
-botao4.grid(row=1, column=0)
-botao4.config(height=10, width=20)
-
-botao5=tk.Button(window)
-botao5.grid(row=1, column=1)
-botao5.config(height=10, width=20)
-
-botao6=tk.Button(window)
-botao6.grid(row=1, column=2)
-botao6.config(height=10, width=20)
-
-botao7=tk.Button(window)
-botao7.grid(row=2, column=0)
-botao7.config(height=10, width=20)
-
-botao8=tk.Button(window)
-botao8.grid(row=2, column=1)
-botao8.config(height=10, width=20)
-
-botao9=tk.Button(window)
-botao9.grid(row=2, column=2)
-botao9.config(height=10, width=20)
-		
-window.mainloop()
+jogo=Tabuleiro()
+jogo.iniciar()
+#jogo.selec_x()
