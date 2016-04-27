0510a3aaa7d02ea0b00d64c20563fecb6e6c644b
matheuscgc <matheuscgc@al.insper.edu.br>
2016-04-26 23:56:05 -0300

    Jogo da velha terminado!

--- Jogo.py
+++ Jogo.py
@@ -9,62 +9,62 @@ Created on Thu Apr 21 20:28:05 2016
 '''
 Jogo da Velha
 '''
-
 class Jogo():
     def __init__(self):
-        #self.jogador = jogador
         self.game = [[0,0,0],[0,0,0],[0,0,0]]
         self.n_zeros = 9       
 								
     def recebe_jogada (self, linha, coluna):
-        if self.n_zeros%2 == 1 and self.n_zeros>=0:
-            self.game[linha][coluna] = 'X'
+        if self.n_zeros%2 == 1 and self.game[linha][coluna]==0:
+            self.game[linha][coluna] = "X"
             self.n_zeros-=1
-        elif self.n_zeros%2 == 0 and self.n_zeros>=0: 
-            self.game[linha][coluna] = 'O'
-            self.n_zeros-=1
-        #return self.n_zeros
+        elif self.n_zeros%2 == 0 and self.game[linha][coluna]==0: 
+            self.game[linha][coluna] = "O"
+            self.n_zeros-=1    
     
-    
-    def verifica_ganhador(self):
-        for i in range(len(self.game)):
-            if self.game[0][i] == self.game[1][i] and self.game[0][i] == self.game[2][i]:
-                if self.game[0][i] == 'X':
-                    return 1
-                elif self.game[i][0] == 'O':
-                    return 2    
-                
-            elif self.game[i] == ['X','X','X']:
-                return 1
-            elif self.game[i] == ['O','O','O']:
-                return 2
-            else:
-                return -1
-                
-        if self.game[0][0] == self.game[1][1] and self.game[0][0] == self.game[2][2]:
-            if self.game[0][0] == 'X':
-                return 1
-            elif self.game[0][0] == 'O':
-                return 2
-            else:
-                return -1
-        elif self.game[0][2] == self.game[1][1] and self.game[0][2] == self.game[2][0]:
-            if self.game[0][2] == 'X':
-                return 1
-            elif self.game[0][2] == 'O':
-                return 2
-            else:
-                return -1
-        elif self.n_zeros == 0:
+    def verifica_ganhador(self):                
+        if self.game[0][0] == "X" and self.game[1][1] == "X" and self.game[2][2] == "X":
+            return 1 
+        elif self.game[0][2] == "X" and self.game[1][1] == "X" and self.game[2][0] == "X":
+            return 1
+        elif self.game[0][0] == "X" and self.game[0][1] == "X" and self.game[0][2] == "X":
+            return 1 
+        elif self.game[1][0] == "X" and self.game[1][1] == "X" and self.game[1][2] == "X":
+            return 1
+        elif self.game[2][0] == "X" and self.game[2][1] == "X" and self.game[2][2] == "X":
+            return 1
+        elif self.game[0][0] == "X" and self.game [1][0] == "X" and self.game[2][0] == "X":
+            return 1
+        elif self.game[0][1] == "X" and self.game[1][1] == "X" and self.game[2][1] == "X":
+            return 1
+        elif self.game[0][2] == "X" and self.game[1][2] == "X" and self.game[2][2] == "X":
+            return 1
+        elif self.game[0][0] == "O" and self.game[1][1] == "O" and self.game[2][2] == "O":
+            return 2
+        elif self.game[0][2] == "O" and self.game[1][1] == "O" and self.game[2][0] == "O":
+            return 2 
+        elif self.game[0][0] == "O" and self.game[0][1] == "O" and self.game[0][2] == "O":
+            return 2
+        elif self.game[1][0] == "O" and self.game[1][1] == "O" and self.game[1][2] == "O":
+            return 2
+        elif self.game[2][0] == "O" and self.game[2][1] == "O" and self.game[2][2] == "O":
+            return 2
+        elif self.game[0][0] == "O" and self.game [1][0] == "O" and self.game[2][0] == "O":
+            return 2
+        elif self.game[0][1] == "O" and self.game[1][1] == "O" and self.game[2][1] == "O":
+            return 2
+        elif self.game[0][2] == "O" and self.game[1][2] == "O" and self.game[2][2] == "O":
+            return 2
+        elif self.n_zeros==0:
             return 0
+        elif self.n_zeros==-1:
+            if self.game.count(0) == 0:
+                return 0
+            else:
+                return -1
         else:
             return -1
-
                 
     
     def limpa_jogadas(self): 
-        
-        if self.verifica_ganhador() >= 0:
-            self.game = [[0,0,0],[0,0,0],[0,0,0]]
- 
-            
+        self.game = [[0,0,0],[0,0,0],[0,0,0]]        
--- Tabuleiro.py
+++ Tabuleiro.py
@@ -5,94 +5,124 @@ class Tabuleiro(Jogo):
 		self.window=tk.Tk()
 		self.window.title("Jogo da velha!")
 		self.joguinho = Jogo()
+		self.cria_tabuleiro()
+
+	def cria_tabuleiro(self):		
+		if self.joguinho.verifica_ganhador() == -1:		
+			prox_jogada=tk.Message()
+			prox_jogada.config(text="Próxima jogada: X")
+			prox_jogada.grid(row=3, sticky = tk.W+tk.E)		
 		
-		prox_jogada=tk.Message()
-		prox_jogada.config(text="Próxima jogada: X")
-		prox_jogada.grid(row=3, sticky = tk.W+tk.E)		
+		elif self.joguinho.verifica_ganhador() == 1:
+			prox_jogada=tk.Message()			
+			prox_jogada.config(text="Vencedor: X. Próxima jogada: O")
+			prox_jogada.grid(row=3, sticky = tk.W+tk.E)
+		
+		elif self.joguinho.verifica_ganhador() == 2:
+			prox_jogada=tk.Message()			
+			prox_jogada.config(text="Vencedor: O. Próxima jogada: X")
+			prox_jogada.grid(row=3, sticky = tk.W+tk.E)
+			
+		elif self.joguinho.verifica_ganhador() == 0:
+			prox_jogada=tk.Message()			
+			prox_jogada.config(text="Empate. O Jogo reinicia, então quem começa agora é quem começou o anterior")
+			prox_jogada.grid(row=3, sticky = tk.W+tk.E)
 		
 		botao1=tk.Button(self.window)
 		botao1.grid(row=0, column=0)
 		botao1.config(height=10, width=20)
-		botao1.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao1, 0, 0))
-#		botao1.bind("<Button-1>", self.joguinho.recebe_jogada(0, 0))
+		botao1.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao1, 0, 0))
 
 		botao2=tk.Button(self.window)
 		botao2.grid(row=0, column=1)
 		botao2.config(height=10, width=20)
-		botao2.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao2, 0, 1))
-#		botao2.bind("<Button-1>", self.joguinho.recebe_jogada(0, 1))
+		botao2.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao2, 0, 1))
 
 		botao3=tk.Button(self.window)
 		botao3.grid(row=0, column=2)
 		botao3.config(height=10, width=20)
-		botao3.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao3, 0, 2))
-#		botao3.bind("<Button-1>", self.joguinho.recebe_jogada(0, 2))
+		botao3.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao3, 0, 2))
 
 		botao4=tk.Button(self.window)
 		botao4.grid(row=1, column=0)
 		botao4.config(height=10, width=20)
-		botao4.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao4, 1, 0))
-#		botao4.bind("<Button-1>", self.joguinho.recebe_jogada(1, 0))
+		botao4.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao4, 1, 0))
 	
 		botao5=tk.Button(self.window)
 		botao5.grid(row=1, column=1)
 		botao5.config(height=10, width=20)
-		botao5.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao5, 1, 1))
-#		botao5.bind("<Button-1>", self.joguinho.recebe_jogada(1, 1))
-
+		botao5.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao5, 1, 1))
+		
 		botao6=tk.Button(self.window)
 		botao6.grid(row=1, column=2)
 		botao6.config(height=10, width=20)
-		botao6.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao6, 1, 2))
-#		botao6.bind("<Button-1>", self.joguinho.recebe_jogada(1, 2))
+		botao6.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao6, 1, 2))
 
 		botao7=tk.Button(self.window)
 		botao7.grid(row=2, column=0)
 		botao7.config(height=10, width=20)
-		botao7.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao7, 2, 0))
-#		botao7.bind("<Button-1>", self.joguinho.recebe_jogada(2, 0))
+		botao7.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao7, 2, 0))
 
 		botao8=tk.Button(self.window)
 		botao8.grid(row=2, column=1)
 		botao8.config(height=10, width=20)
-		botao8.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao8, 2, 1))
-#		botao8.bind("<Button-1>", self.joguinho.recebe_jogada(2, 1))
+		botao8.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao8, 2, 1))
 
 		botao9=tk.Button(self.window)
 		botao9.grid(row=2, column=2)
 		botao9.config(height=10, width=20)
-		botao9.configure(command=lambda: Tabuleiro.unificar(self, prox_jogada, botao9, 2, 2))
-#		botao9.bind("<Button-1>", self.joguinho.recebe_jogada(2, 2))	
-
-	
+		botao9.configure(command=lambda: Tabuleiro.alterna(self, prox_jogada, botao9, 2, 2))
+		
 	def iniciar(self):
 		self.window.mainloop()
-		
-	def unificar(self, prox_jogada, botao, linha, coluna):
-		self.alterna(prox_jogada, botao)
-		self.joguinho.recebe_jogada(linha, coluna)
-
-
-	def alterna(self, prox_jogada, botao):
-		if self.joguinho.n_zeros%2 == 1 and self.joguinho.n_zeros>=0:
-			botao["text"]="O"
-			prox_jogada.config(text="Próxima jogada: X")
-			self.joguinho.n_zeros-=1			
 			
-		elif self.joguinho.n_zeros%2==0 and self.joguinho.n_zeros>=0: 
-			botao["text"]="X"
-			prox_jogada.config(text="Próxima jogada: O")
-			self.joguinho.n_zeros-=1
-		
-		
-		
+	def alterna(self, prox_jogada, botao, linha, coluna):
+		if self.joguinho.verifica_ganhador() == -1:		
+			if self.joguinho.n_zeros%2 == 1 and self.joguinho.game[linha][coluna]==0:
+				botao["text"]="X"
+				prox_jogada.config(text="Próxima jogada: O")
+				self.joguinho.recebe_jogada(linha, coluna)
+				if self.joguinho.verifica_ganhador() == 1:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 8			
+					self.joguinho.limpa_jogadas()
+			
+				elif self.joguinho.verifica_ganhador() == 2:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 9			
+					self.joguinho.limpa_jogadas()
+					
+				elif self.joguinho.verifica_ganhador() == 0:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 9
+					self.joguinho.limpa_jogadas()
+				
+			elif self.joguinho.n_zeros%2 == 0 and self.joguinho.game[linha][coluna]==0: 
+				botao["text"]="O"			
+				prox_jogada.config(text="Próxima jogada: X")
+				self.joguinho.recebe_jogada(linha, coluna)
+				self.joguinho.verifica_ganhador()
+				if self.joguinho.verifica_ganhador() == 1:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 8			
+					self.joguinho.limpa_jogadas()
+			
+				elif self.joguinho.verifica_ganhador() == 2:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 9			
+					self.joguinho.limpa_jogadas()
+					
+				elif self.joguinho.verifica_ganhador() == 0:
+					Tabuleiro.cria_tabuleiro(self)					
+					self.joguinho.n_zeros = 9
+					self.joguinho.limpa_jogadas()
+					
 jogo=Tabuleiro()
 jogo.iniciar()
 
 jogo.joguinho.verifica_ganhador()
 print (jogo.joguinho.game)
 print (jogo.joguinho.verifica_ganhador())
-jogo.joguinho.limpa_jogadas()
 
 print (jogo.joguinho.game)
 print (jogo.joguinho.n_zeros)
